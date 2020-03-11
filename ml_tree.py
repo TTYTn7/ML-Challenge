854f126e9321d5cf99f065aada13a3b119637a0e
Bobby <themerrysuicide@gmail.com>
2020-03-11 05:41:03 +0200

    Added education, last try 71.9%

--- ml_tree.py
+++ ml_tree.py
@@ -17,7 +17,7 @@ with open('row_names.txt', 'r') as f:
 		line = f.readline().strip()
 
 income_set = pd.read_csv('census-income.csv', names=col_names)
-print (income_set.info())
+#print (income_set.info())
 
 # Replacing the default placeholder value ('Not in universe') with NaN
 #for x in col_names:
@@ -28,21 +28,33 @@ print (income_set.info())
 # Selecting all of the numerical fields as predictors, 'aside of instance weight'
 # TO DO:
 #		Find a way to add categorical (string) data as predictors
-X = income_set.select_dtypes(include='int64')#.drop('instance weight', axis=1)
+#X = income_set.select_dtypes(include='int64')
+X = pd.get_dummies(income_set[['age', 'detailed industry recode', 
+				'detailed occupation recode', 'education', 'wage per hour', 
+				'capital gains', 'capital losses','dividends from stocks', 
+				'num persons worked for employer', 'own business or self employed', 
+				'veterans benefits', 'weeks worked in year', 'year']])
+#print (X.info())
 y = income_set['income class'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05, random_state = 42, stratify=y)
 
 # Adding the test set, again only the numeric values
-pred_set = pd.read_csv('census-income-test.csv', names=col_names).select_dtypes(include='int64')
+pred_set = pd.read_csv('census-income-test.csv', names=col_names)#.select_dtypes(include='int64')
+pred_set_2 = pd.get_dummies(pred_set[['age', 'detailed industry recode', 
+				'detailed occupation recode', 'education', 'wage per hour', 
+				'capital gains', 'capital losses','dividends from stocks', 
+				'num persons worked for employer', 'own business or self employed', 
+				'veterans benefits', 'weeks worked in year', 'year']])
+#print (pred_set_2.info())
 
 
-dt = DecisionTreeClassifier(max_depth=24, random_state =42)
+dt = DecisionTreeClassifier(max_depth=36, random_state =42)
 dt.fit(X_train, y_train)
 print (dt.score(X_test, y_test))
 
 
-prediction = pd.DataFrame(dt.predict(pred_set))
+prediction = pd.DataFrame(dt.predict(pred_set_2))
 
 #print (prediction.info())
 
@@ -50,7 +62,7 @@ prediction = pd.DataFrame(dt.predict(pred_set))
 dictionary = {' - 50000.': 0, ' 50000+.': 1}
 prediction[0] = prediction[0].map(dictionary)
 
-prediction.to_csv('prediction.csv')
+prediction.to_csv('prediction.csv', header=['income class'])
 # TO DO:
 #		Add the 'index' and 'income class' col names to the prediction.csv upon saving.
 #		Currently has to be done manually
