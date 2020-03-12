f82287043236f5e60a12920e7b5f7c4b656f19b5
Bobby <Bozhidar.dimitrov.95@Gmail.com>
2020-03-12 23:47:59 +0100

    Forest model, 84%

--- ml_tree.py
+++ ml_tree.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 col_names = []
 # Adding names for the columns of the input data
@@ -16,7 +17,7 @@ with open('row_names.txt', 'r') as f:
 		col_names.append(line)
 		line = f.readline().strip()
 
-income_set = pd.read_csv('census-income.csv', names=col_names)
+training_set = pd.read_csv('census-income.csv', names=col_names)
 #print (income_set.info())
 
 # Replacing the default placeholder value ('Not in universe') with NaN
@@ -29,40 +30,65 @@ income_set = pd.read_csv('census-income.csv', names=col_names)
 # TO DO:
 #		Find a way to add categorical (string) data as predictors
 #X = income_set.select_dtypes(include='int64')
-X = pd.get_dummies(income_set[['age', 'detailed industry recode', 
+X = pd.get_dummies(training_set[['age', 'detailed industry recode', 
 				'detailed occupation recode', 'education', 'wage per hour', 
-				'capital gains', 'capital losses','dividends from stocks', 
-				'num persons worked for employer', 'own business or self employed', 
-				'veterans benefits', 'weeks worked in year', 'year']])
+				'capital gains', 'dividends from stocks', 'major occupation code',
+				'major industry code', 'sex', 'class of worker',
+				'num persons worked for employer', 'weeks worked in year']])
 #print (X.info())
-y = income_set['income class'].values
+y = training_set['income class'].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05, random_state = 42, stratify=y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.05, random_state = 2, stratify=y)
+#X_test = pd.get_dummies(test_set[['age', 'detailed industry recode', 
+				#'detailed occupation recode', 'education', 'wage per hour', 
+				#'capital gains', 'capital losses','dividends from stocks', 
+				#'num persons worked for employer', 'own business or self employed', 
+				#'veterans benefits', 'weeks worked in year']])
+#y_test = test_set['income class'].values
 
 # Adding the test set, again only the numeric values
-pred_set = pd.read_csv('census-income-test.csv', names=col_names)#.select_dtypes(include='int64')
-pred_set_2 = pd.get_dummies(pred_set[['age', 'detailed industry recode', 
+test_set = pd.read_csv('census-income-test.csv', names=col_names)#.select_dtypes(include='int64')
+test_set_2 = pd.get_dummies(test_set[['age', 'detailed industry recode', 
 				'detailed occupation recode', 'education', 'wage per hour', 
-				'capital gains', 'capital losses','dividends from stocks', 
-				'num persons worked for employer', 'own business or self employed', 
-				'veterans benefits', 'weeks worked in year', 'year']])
+				'capital gains', 'dividends from stocks', 'major occupation code',
+				'major industry code', 'sex', 'class of worker',
+				'num persons worked for employer', 'weeks worked in year']])
 #print (pred_set_2.info())
 
 
-dt = DecisionTreeClassifier(max_depth=36, random_state =42)
-dt.fit(X_train, y_train)
-print (dt.score(X_test, y_test))
+#Single tree
+# dt = DecisionTreeClassifier(max_depth=42, random_state =2)
+# dt.fit(X_train, y_train)
+# print ('Tree score:')
+# print (dt.score(X_test, y_test))
 
+#Forest
+forest = RandomForestClassifier(n_estimators=200, bootstrap=True, max_features='sqrt')
+forest.fit(X_train, y_train)
+print ('Forest score:')
+print (forest.score(X_test, y_test))
 
-prediction = pd.DataFrame(dt.predict(pred_set_2))
+# prediction = pd.DataFrame(dt.predict(test_set_2))
+prediction_forest = pd.DataFrame(forest.predict(test_set_2))
 
+# fi = pd.DataFrame({'feature': list(training_set.columns), 'importance': dt.feature_importances_}).\
+#                     sort_values('importance', ascending = False)
+
+# fi2 = pd.DataFrame({'feature': list(training_set.columns), 'importance': forest.feature_importances_}).\
+# 					sort_values('importance', ascending = False)
+
+# print ('Feature importance tree:')
+# print (fi.head())
+
+# print ('Feature importance forest:')
+# print (fi2.head())
 #print (prediction.info())
 
 # Replacing the default vals with the ones required by the solution format
 dictionary = {' - 50000.': 0, ' 50000+.': 1}
-prediction[0] = prediction[0].map(dictionary)
+prediction_forest[0] = prediction_forest[0].map(dictionary)
 
-prediction.to_csv('prediction.csv', header=['income class'])
+prediction_forest.to_csv('prediction.csv', header=['income class'])
 # TO DO:
 #		Add the 'index' and 'income class' col names to the prediction.csv upon saving.
 #		Currently has to be done manually
